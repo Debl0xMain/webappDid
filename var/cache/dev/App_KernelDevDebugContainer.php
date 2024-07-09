be863880d973ff89bccaea56fd54c4431ca71658
Debl0xMain <deblox160999@gmail.com>
2024-07-09 09:24:41 +0200

    up

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerUgWHGD2\App_KernelDevDebugContainer([
     'container.build_hash' => 'UgWHGD2',
-    'container.build_id' => '6b13f22e',
-    'container.build_time' => 1720509547,
+    'container.build_id' => 'd3b232b8',
+    'container.build_time' => 1720509815,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerUgWHGD2');
